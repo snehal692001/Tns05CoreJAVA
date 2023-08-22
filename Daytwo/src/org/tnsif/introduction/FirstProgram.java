8ca3d1d338106ab499f0c9e8fb44bb76e9fa9a50
salgu <salgu@LAPTOP-HLKGEVP0>
2023-08-22 22:46:18 +0530

    first commit

---
+++ Daytwo/src/org/tnsif/introduction/FirstProgram.java
@@ -0,0 +1,11 @@
+package org.tnsif.introduction;
+//Program to demonstrate on simple java program
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		
+     System.out.println("Hello World!");
+
+	}
+
+}
